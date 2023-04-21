e417c356bab6f600b94ed72d71238ccfb2a9582b
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2023-04-21 12:54:04 +0200

    🐛 fix(create-markdown.yml): add --no-cache flag to dotnet tool install command 🐛 fix(MarkdownOutputFormatter.cs): add missing using statement for CultureInfo ✨ feat(MarkdownOutputFormatter.cs): add mermaidjs charts for cost visualization The `--no-cache` flag is added to the `dotnet tool install` command to ensure that the latest version of the Azure Cost CLI is installed. The missing `using` statement for `CultureInfo` is added to the `MarkdownOutputFormatter.cs` file. The `MarkdownOutputFormatter.cs` file is updated to include mermaidjs charts for cost visualization. The charts include a gantt chart for the last 7 days and a forecast, a pie chart for cost by service, a pie chart for cost by location, and a pie chart for cost by resource group.

--- src/Commands/ShowCommand/OutputFormatters/MarkdownOutputFormatter.cs
+++ src/Commands/ShowCommand/OutputFormatters/MarkdownOutputFormatter.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AzureCostCli.CostApi;
 using AzureCostCli.Infrastructure;
 
@@ -5,7 +6,7 @@ namespace AzureCostCli.Commands.ShowCommand.OutputFormatters;
 
 public class MarkdownOutputFormatter : OutputFormatter
 {
-    public override Task WriteOutput(ShowSettings settings, 
+    public override Task WriteOutput(ShowSettings settings,
         IEnumerable<CostItem> costs,
         IEnumerable<CostItem> forecastedCosts,
         IEnumerable<CostNamedItem> byServiceNameCosts,
@@ -27,10 +28,13 @@ public class MarkdownOutputFormatter : OutputFormatter
         };
 
         var currency = costs.FirstOrDefault()?.Currency;
+        var culture = CultureInfo.GetCultureInfo("en-US");
+        var todaysDate = DateOnly.FromDateTime(DateTime.UtcNow);
 
         Console.WriteLine("# Azure Cost Overview");
         Console.WriteLine();
-        Console.WriteLine($"> Details for subscription id `{settings.Subscription}` from **{costs.Min(a => a.Date)}** to **{costs.Max(a => a.Date)}**");
+        Console.WriteLine(
+            $"> Details for subscription id `{settings.Subscription}` from **{costs.Min(a => a.Date)}** to **{costs.Max(a => a.Date)}**");
         Console.WriteLine();
         Console.WriteLine("## Totals");
         Console.WriteLine();
@@ -40,6 +44,40 @@ public class MarkdownOutputFormatter : OutputFormatter
         Console.WriteLine($"|Yesterday|{output.costs.yesterdayCost:N2} {currency}|");
         Console.WriteLine($"|Last 7 days|{output.costs.lastSevenDaysCost:N2} {currency}|");
         Console.WriteLine($"|Last 30 days|{output.costs.lastThirtyDaysCost:N2} {currency}|");
+
+        // Generate a gantt chart using mermaidjs
+        Console.WriteLine();
+        Console.WriteLine("```mermaid");
+        Console.WriteLine("gantt");
+        Console.WriteLine("   title Last 7 days");
+        Console.WriteLine("   dateFormat  X");
+        Console.WriteLine("   axisFormat %s");
+
+        var lastSevenDays = costs.Where(x => x.Date >= todaysDate.AddDays(-7)).OrderBy(x => x.Date).ToList();
+        foreach (var day in lastSevenDays)
+        {
+            Console.WriteLine($"   section {day.Date.ToString("dd MMM")}");
+            Console.WriteLine($"   {currency} {Math.Round(day.Cost, 2)} :0, {Math.Round(day.Cost* 100, 0) }");
+        }
+
+        Console.WriteLine("```");
+
+        // And a similar one for the forecast
+        Console.WriteLine();
+        Console.WriteLine("```mermaid");
+        Console.WriteLine("gantt");
+        Console.WriteLine("   title Forecast");
+        Console.WriteLine("   dateFormat  X");
+        Console.WriteLine("   axisFormat %s");
+
+        var lastSevenDaysForecast = forecastedCosts.Where(x => x.Date >= todaysDate.AddDays(-7)).OrderBy(x => x.Date).ToList();
+        foreach (var day in lastSevenDaysForecast)
+        {
+            Console.WriteLine($"   section {day.Date.ToString("dd MMM")}");
+            Console.WriteLine($"   {currency} {Math.Round(day.Cost, 2)} :0, {Math.Round(day.Cost* 100, 0) }");
+        }
+
+        Console.WriteLine("```");
         
         Console.WriteLine();
         Console.WriteLine("## By Service Name");
@@ -50,6 +88,17 @@ public class MarkdownOutputFormatter : OutputFormatter
         {
             Console.WriteLine($"|{cost.ItemName}|{cost.Cost:N2} {currency}|");
         }
+        
+        // Create a pie chart using mermaidjs
+        Console.WriteLine();
+        Console.WriteLine("```mermaid");
+        Console.WriteLine("pie");
+        Console.WriteLine("   title Cost by service");
+        foreach (var cost in byServiceNameCosts.TrimList(threshold: settings.OthersCutoff))
+        {
+            Console.WriteLine($"   \"{cost.ItemName}\" : {cost.Cost.ToString("N2", culture)}");
+        }
+        Console.WriteLine("```");
 
         Console.WriteLine();
         Console.WriteLine("## By Location");
@@ -60,6 +109,17 @@ public class MarkdownOutputFormatter : OutputFormatter
         {
             Console.WriteLine($"|{cost.ItemName}|{cost.Cost:N2} {currency}|");
         }
+        
+        // Create a pie chart using mermaidjs
+        Console.WriteLine();
+        Console.WriteLine("```mermaid");
+        Console.WriteLine("pie");
+        Console.WriteLine("   title Cost by location");
+        foreach (var cost in byLocationCosts.TrimList(threshold: settings.OthersCutoff))
+        {
+            Console.WriteLine($"   \"{cost.ItemName}\" : {cost.Cost.ToString("N2", culture)}");
+        }
+        Console.WriteLine("```");
 
         Console.WriteLine();
         Console.WriteLine("## By Resource Group");
@@ -71,9 +131,20 @@ public class MarkdownOutputFormatter : OutputFormatter
             Console.WriteLine($"|{cost.ItemName}|{cost.Cost:N2} {currency}|");
         }
 
+        // Generate a pie chart using mermaidjs
+        Console.WriteLine();
+        Console.WriteLine("```mermaid");
+        Console.WriteLine("pie");
+        Console.WriteLine("   title Cost by resource group");
+        foreach (var cost in byResourceGroupCosts.TrimList(threshold: settings.OthersCutoff))
+        {
+            Console.WriteLine($"   \"{cost.ItemName}\" : {cost.Cost.ToString("N2", culture)}");
+        }
+        Console.WriteLine("```");
+        
         Console.WriteLine();
         Console.WriteLine($"<sup>Generated at {DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}</sup>");
-        
+
         return Task.CompletedTask;
     }
 }
