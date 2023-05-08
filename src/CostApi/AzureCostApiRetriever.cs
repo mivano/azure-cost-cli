e84ac3700b27de929075dffde0b8a12d33ddaf91
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2023-05-08 23:52:14 +0200

    Handle a failing forecast

--- src/CostApi/AzureCostApiRetriever.cs
+++ src/CostApi/AzureCostApiRetriever.cs
@@ -13,48 +13,52 @@ namespace AzureCostCli.CostApi;
 
 public class AzureCostApiRetriever : ICostRetriever
 {
-    
     private readonly HttpClient _client;
     private bool _tokenRetrieved;
-    
+
     public AzureCostApiRetriever(IHttpClientFactory httpClientFactory)
     {
         _client = httpClientFactory.CreateClient("CostApi");
     }
-    
+
     public static IAsyncPolicy<HttpResponseMessage> GetRetryAfterPolicy()
     {
         return Policy.HandleResult<HttpResponseMessage>
-                (msg => msg.Headers.TryGetValues("x-ms-ratelimit-microsoft.costmanagement-entity-retry-after", out var _))
+            (msg => msg.Headers.TryGetValues("x-ms-ratelimit-microsoft.costmanagement-entity-retry-after",
+                out var _))
             .WaitAndRetryAsync(
                 retryCount: 3,
-                sleepDurationProvider: (_, response, _) => response.Result.Headers.TryGetValues("x-ms-ratelimit-microsoft.costmanagement-entity-retry-after", out var seconds )? TimeSpan.FromSeconds(int.Parse(seconds.First())) : TimeSpan.FromSeconds(5) ,
-                onRetryAsync: (msg,time , retries, context) => Task.CompletedTask
+                sleepDurationProvider: (_, response, _) =>
+                    response.Result.Headers.TryGetValues("x-ms-ratelimit-microsoft.costmanagement-entity-retry-after",
+                        out var seconds)
+                        ? TimeSpan.FromSeconds(int.Parse(seconds.First()))
+                        : TimeSpan.FromSeconds(5),
+                onRetryAsync: (msg, time, retries, context) => Task.CompletedTask
             );
     }
-    
+
     private async Task RetrieveToken(bool includeDebugOutput)
     {
         if (_tokenRetrieved)
             return;
-        
+
         // Get the token by using the DefaultAzureCredential
         var tokenCredential = new ChainedTokenCredential(
             new AzureCliCredential(),
             new DefaultAzureCredential());
-        
+
         if (includeDebugOutput)
             AnsiConsole.WriteLine($"Using token credential: {tokenCredential.GetType().Name} to fetch a token.");
-        
+
         var token = await tokenCredential.GetTokenAsync(new TokenRequestContext(new[]
             { $"https://management.azure.com/.default" }));
 
         if (includeDebugOutput)
             AnsiConsole.WriteLine($"Token retrieved and expires at: {token.ExpiresOn}");
-        
+
         // Set as the bearer token for the HTTP client
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
-        
+
         _tokenRetrieved = true;
     }
 
@@ -63,17 +67,18 @@ public class AzureCostApiRetriever : ICostRetriever
         await RetrieveToken(includeDebugOutput);
 
         if (includeDebugOutput)
-        { 
+        {
             AnsiConsole.WriteLine($"Retrieving data from {uri} using the following payload:");
             AnsiConsole.Write(new JsonText(JsonSerializer.Serialize(payload)));
             AnsiConsole.WriteLine();
         }
 
         var response = await _client.PostAsJsonAsync(uri, payload);
-        
+
         if (includeDebugOutput)
         {
-            AnsiConsole.WriteLine($"Response status code is {response.StatusCode} and got payload size of {response.Content.Headers.ContentLength}");
+            AnsiConsole.WriteLine(
+                $"Response status code is {response.StatusCode} and got payload size of {response.Content.Headers.ContentLength}");
             if (!response.IsSuccessStatusCode)
             {
                 AnsiConsole.WriteLine($"Response content: {await response.Content.ReadAsStringAsync()}");
@@ -83,7 +88,7 @@ public class AzureCostApiRetriever : ICostRetriever
         response.EnsureSuccessStatusCode();
         return response;
     }
-    
+
     public async Task<IEnumerable<CostItem>> RetrieveCosts(bool includeDebugOutput, Guid subscriptionId,
         TimeframeType timeFrame, DateOnly from, DateOnly to)
     {
@@ -149,7 +154,6 @@ public class AzureCostApiRetriever : ICostRetriever
         return items;
     }
 
-  
 
     public async Task<IEnumerable<CostNamedItem>> RetrieveCostByServiceName(bool includeDebugOutput,
         Guid subscriptionId, TimeframeType timeFrame, DateOnly from, DateOnly to)
@@ -312,14 +316,15 @@ public class AzureCostApiRetriever : ICostRetriever
 
         return items;
     }
-    
-    public async Task<IEnumerable<CostNamedItem>> RetrieveCostByResourceGroup(bool includeDebugOutput, Guid subscriptionId,
+
+    public async Task<IEnumerable<CostNamedItem>> RetrieveCostByResourceGroup(bool includeDebugOutput,
+        Guid subscriptionId,
         TimeframeType timeFrame, DateOnly from, DateOnly to)
     {
         var uri = new Uri(
             $"/subscriptions/{subscriptionId}/providers/Microsoft.CostManagement/query?api-version=2021-10-01&$top=5000",
             UriKind.Relative);
-        //   "type":"Dimension","name":"ResourceGroupName"},{"type":"Dimension","name":"ChargeType"},{"type":"Dimension","name":"PublisherType"
+
         var payload = new
         {
             type = "ActualCost",
@@ -399,7 +404,8 @@ public class AzureCostApiRetriever : ICostRetriever
         return items;
     }
 
-    public async Task<IEnumerable<CostItem>> RetrieveForecastedCosts(bool includeDebugOutput, Guid subscriptionId, TimeframeType timeFrame, DateOnly from, DateOnly to)
+    public async Task<IEnumerable<CostItem>> RetrieveForecastedCosts(bool includeDebugOutput, Guid subscriptionId,
+        TimeframeType timeFrame, DateOnly from, DateOnly to)
     {
         var uri = new Uri(
             $"/subscriptions/{subscriptionId}/providers/Microsoft.CostManagement/forecast?api-version=2021-10-01&$top=5000",
@@ -437,33 +443,49 @@ public class AzureCostApiRetriever : ICostRetriever
                 }
             }
         };
-        var response = await ExecuteCallToCostApi(includeDebugOutput, payload, uri);
-
-        CostQueryResponse? content = await response.Content.ReadFromJsonAsync<CostQueryResponse>();
 
         var items = new List<CostItem>();
-        foreach (var row in content.properties.rows)
+
+        try
         {
-            var date = DateOnly.ParseExact(row[1].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
-            var value = double.Parse(row[0].ToString(), CultureInfo.InvariantCulture);
+            // Allow this one to fail, as it is not supported for all subscriptions
+            var response = await ExecuteCallToCostApi(includeDebugOutput, payload, uri);
 
-            var currency = row[3].ToString();
+            CostQueryResponse? content = await response.Content.ReadFromJsonAsync<CostQueryResponse>();
 
-            var costItem = new CostItem(date, value, value, currency);
-            items.Add(costItem);
+
+            foreach (var row in content.properties.rows)
+            {
+                var date = DateOnly.ParseExact(row[1].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
+                var value = double.Parse(row[0].ToString(), CultureInfo.InvariantCulture);
+
+                var currency = row[3].ToString();
+
+                var costItem = new CostItem(date, value, value, currency);
+                items.Add(costItem);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Eat exception, we log anyway with the debug output
+            if (includeDebugOutput)
+            {
+                AnsiConsole.WriteException(ex);
+                AnsiConsole.WriteLine("Ignoring this exception, as it is not supported for all subscriptions.");
+            }
         }
 
         return items;
     }
 
-    public async Task<IEnumerable<CostResourceItem>> RetrieveCostForResources(bool includeDebugOutput, Guid subscriptionId, TimeframeType timeFrame, DateOnly from,
+    public async Task<IEnumerable<CostResourceItem>> RetrieveCostForResources(bool includeDebugOutput,
+        Guid subscriptionId, TimeframeType timeFrame, DateOnly from,
         DateOnly to)
     {
-        
         var uri = new Uri(
             $"/subscriptions/{subscriptionId}/providers/Microsoft.CostManagement/query?api-version=2021-10-01&$top=5000",
             UriKind.Relative);
-            
+
         var payload = new
         {
             type = "ActualCost",
@@ -491,7 +513,7 @@ public class AzureCostApiRetriever : ICostRetriever
                         function = "Sum"
                     }
                 },
-                include = new[] { "Tags"},
+                include = new[] { "Tags" },
                 grouping = new[]
                 {
                     new
@@ -540,13 +562,12 @@ public class AzureCostApiRetriever : ICostRetriever
                         name = "Meter"
                     }
                 },
-               
             }
         };
         var response = await ExecuteCallToCostApi(includeDebugOutput, payload, uri);
 
         CostQueryResponse? content = await response.Content.ReadFromJsonAsync<CostQueryResponse>();
-        
+
         var items = new List<CostResourceItem>();
         foreach (JsonElement row in content.properties.rows)
         {
@@ -563,14 +584,13 @@ public class AzureCostApiRetriever : ICostRetriever
             string meter = row[10].GetString();
             string[] tags = row[11].EnumerateArray().Select(tag => tag.GetString()).ToArray();
             string currency = row[12].GetString();
-    
-            CostResourceItem item = new CostResourceItem(cost, costUSD, resourceId, resourceType, resourceLocation, 
+
+            CostResourceItem item = new CostResourceItem(cost, costUSD, resourceId, resourceType, resourceLocation,
                 chargeType, resourceGroupName, publisherType, serviceName, serviceTier, meter, tags, currency);
-    
+
             items.Add(item);
         }
 
         return items;
-        
     }
 }
