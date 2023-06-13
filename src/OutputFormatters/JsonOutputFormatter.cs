51e891b6a8ebee9281eadac9be8b3269c4eedbc5
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2023-06-13 16:15:41 +0200

    support for dateonly type convertors

--- src/OutputFormatters/JsonOutputFormatter.cs
+++ src/OutputFormatters/JsonOutputFormatter.cs
@@ -114,12 +114,23 @@ public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
 {
     public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        return DateOnly.FromDateTime(reader.GetDateTime());
+        return DateOnly.Parse(reader.GetString()!);
+    }
+
+    public override DateOnly ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.Parse(reader.GetString()!);
     }
 
     public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
     {
         var isoDate = value.ToString("O");
         writer.WriteStringValue(isoDate);
+    }
+
+    public override void WriteAsPropertyName(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        var isoDate = value.ToString("O");
+        writer.WritePropertyName(isoDate);
     }
 }
