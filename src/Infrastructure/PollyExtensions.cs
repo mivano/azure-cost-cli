f7cc9a0b0aa3937b709c0b5dabd1ff13f97d5117
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2024-06-15 14:24:46 +0200

    Handle retries in a more generic manner

--- src/Infrastructure/PollyExtensions.cs
+++ src/Infrastructure/PollyExtensions.cs
@@ -3,24 +3,23 @@ using Polly;
 
 namespace AzureCostCli.Infrastructure;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 public class PollyExtensions
 {
-    private static string RetryAfterHeader = "x-ms-ratelimit-microsoft.costmanagement-clienttype-retry-after";
-    private static string RetryAfterHeader2 = "x-ms-ratelimit-microsoft.costmanagement-entity-retry-after";
-
     public static IAsyncPolicy<HttpResponseMessage> GetRetryAfterPolicy()
     {
         return Policy.HandleResult<HttpResponseMessage>
                 (msg => msg.StatusCode == HttpStatusCode.TooManyRequests)
             .WaitAndRetryAsync(
-                retryCount: 3,
+                retryCount: 5,
                 sleepDurationProvider: (_, response, _) =>
-                    response.Result.Headers.TryGetValues(RetryAfterHeader,
-                        out var seconds)
-                        ? TimeSpan.FromSeconds(int.Parse(seconds.First()))
-                        :  response.Result.Headers.TryGetValues(RetryAfterHeader2,
-                            out var seconds2)
-                            ? TimeSpan.FromSeconds(int.Parse(seconds2.First())): TimeSpan.FromSeconds(5),
+                {
+                    var retryAfterHeader = 
+                        response.Result.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant().Contains("retry-after"));
+                    return retryAfterHeader.Key != null && int.TryParse(retryAfterHeader.Value.First(), out var seconds)
+                        ? TimeSpan.FromSeconds(seconds)
+                        : TimeSpan.FromSeconds(5);
+                },
                 onRetryAsync: (msg, time, retries, context) => Task.CompletedTask
             );
     }
